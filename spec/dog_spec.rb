9e28ed0dc72520ca24f4c8b3fd6884cc2f90fb0e
Max Jacobson <max@hardscrabble.net>
2016-11-14 18:36:38 -0500

    Decrease coverage

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -6,11 +6,4 @@ RSpec.describe Dog do
       expect(Dog.new("Milo").name).to eq("Milo!!!")
     end
   end
-
-  describe "#age_in_dog_years" do
-    it "is 45 for some reason" do
-      expect(Dog.new("Milo").age_in_dog_years).to eq 45
-      expect(Dog.new("Milo").age_in_dog_years).to eq 45
-    end
-  end
 end
