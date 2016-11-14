9c1f4fc882518ce29b3e2132033964f40142642b
Max Jacobson <max@hardscrabble.net>
2016-11-14 18:33:49 -0500

    fix tests

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe Dog do
 
   describe "#age_in_dog_years" do
     it "is 45 for some reason" do
-      expect(Dog.new("Milo").age_in_dog_years).to eq 43
-      expect(Dog.new("Milo").age_in_dog_years).to eq 43
+      expect(Dog.new("Milo").age_in_dog_years).to eq 45
+      expect(Dog.new("Milo").age_in_dog_years).to eq 45
     end
   end
 end
